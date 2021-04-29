086b5a2346d3a085cab4ceb6ba43aabf1c7c860d
yzMa <2641007740@qq.com>
2021-04-29 11:32:15 +0800

    x

--- socket-parent/socket-client/src/main/java/com/myz/socket/client/tcp/Client.java
+++ socket-parent/socket-client/src/main/java/com/myz/socket/client/tcp/Client.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 
 public class Client {
 
-        public static void main(String[] args) throws IOException, InterruptedException {
+        public static void main(String[] args) throws IOException,InterruptedException{
             Socket socket = new Socket("localhost", 12345);
 //            socket.setKeepAlive(true);
             socket.setSendBufferSize(8192);
@@ -19,14 +19,21 @@ public class Client {
             os.write("get test-key".getBytes("UTF-8"));
             os.flush(); // 写完后server立马返回，这里直接sleep 不读取流中的数据
 
-            Thread.sleep(155 * 1000L);
+            Thread.sleep(55 * 1000L);
             os.write("get test-key".getBytes("UTF-8"));
             os.flush();
 
             byte[] bytes = new byte[1024];
 //            is.read(bytes);// 读取一次
 //            PrintUtil.print(" received from server: " + new String(bytes, "UTF-8").trim());
-            while (is.read(bytes) > -1) { // 上面两次写入的流的数据，server立刻返回了，只是未读而已，这里开始读取，由于Server返回的数据少，直接一次读取了
+            while (true) {
+                try {
+                    if (!(is.read(bytes) > -1)) break;
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                    System.out.println("抓紧重新连接");
+                    Thread.sleep(1000);
+                } // 上面两次写入的流的数据，server立刻返回了，只是未读而已，这里开始读取，由于Server返回的数据少，直接一次读取了
                 PrintUtil.print(" received from server: " + new String(bytes, "UTF-8").trim());
                 bytes = new byte[1024];
             }
